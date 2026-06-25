c1eb1e28839b4c5b25f3672807eb6fd0c6c62e22
Deepti Salvi <deepti.more2@gmail.com>
2026-06-25 14:22:52 -0400

    Upload Python101 folder with 2 python files into github

---
+++ Hello.py
@@ -0,0 +1,32 @@
+# ---- This is a string example
+'''
+This is the long text section
+you can add multiple line
+here is the end.
+'''
+print('Hello World!')
+print("This is python class")
+#-- This is a Integer and float
+print(2*2)
+iVar = 8
+print(iVar*7)
+dNum= 3.4
+print(dNum * 4)
+# --example of boolean
+bFaviourate = False
+print(bFaviourate)
+print(type("TExt"))
+print(type(23))
+print(type(False))
+print(2/6)
+print(10%5)
+print(3**2)
+print(3^2)
+ilength=342
+iwidth=56
+strFavoriateColor = input("What's Your favouriate color?")
+print("Nice Choice!" + strFavoriateColor )
+print(f"This is the text concatnating with another string :{strFavoriateColor}")
+print(12, 34 ,45,sep="*")
+# create a customer receipt
+print(f"jdshjshf {round(dNum)}")
